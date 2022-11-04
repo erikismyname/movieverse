de6bda6c381126c64dcb9ee30437db6189e36280
Erik Ivanov <ivnverik@gmail.com>
2022-11-04 22:01:44 +0200

    add a home route

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    }
+  ]
 });
 
 export default router;
